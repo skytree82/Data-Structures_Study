cf9bb0d57a72f33520c3133ef5f4d5b231932a29
skytree82 <77150791+skytree82@users.noreply.github.com>
2023-02-19 16:30:13 +0900

    Update PriorityQueue.h

--- Heap/PriorityQueue.h
+++ Heap/PriorityQueue.h
@@ -1,26 +1,31 @@
 #ifndef __PRIORITY_QUEUE_H__
 #define __PRIORITY_QUEUE_H__
 
+#include <string>
 #include "Heap.h"
 
+using namespace std;
+
 template <class T>
-class PriorityQueue {
+class PriorityQueue{
 private:
-	Heap<T> heapQueue;
+	Heap<T>* heapQueue;
 public:
-	PriorityQueue() {}
+	PriorityQueue(bool (*_cmp)(T senior, T subord)){
+		heapQueue = new Heap<T>(_cmp);
+	}
 
 	bool PQEmpty() {
-		if (heapQueue.HeapEmpty()) return true;
+		if (heapQueue->HeapEmpty()) return true;
 		else return false;
 	}
 
 	void PQEnqueue(T data) {
-		heapQueue.HeapInsert(data);
+		heapQueue->HeapInsert(data);
 	}
 
 	T PQDequeue() {
-		return heapQueue.HeapDelete();
+		return heapQueue->HeapDelete();
 	}
 };
 #endif // !__PRIORITY_QUEUE_H__
