7a57dfdea694a91d59a9a1d27b2ee30057985371
skytree82 <77150791+skytree82@users.noreply.github.com>
2023-03-08 20:59:28 +0900

    Update BinaryTree.h

--- Binary Search Tree/BinaryTree.h
+++ Binary Search Tree/BinaryTree.h
@@ -19,15 +19,15 @@ public:
 		right = NULL;
 	}
 
-	BTree<T>* GetLeftTree() {
+	BTree<T>* GetLeftTree() {  // 왼쪽 트리의 루트노드 메모리 주소 반환
 		return left;
 	}
 
-	BTree<T>* GetRightTree() {
+	BTree<T>* GetRightTree() {  // 오른쪽 트리의 루트노드 메모리 주소 반환
 		return right;
 	}
 
-	void MakeLeftSubTree(BTree<T>* bt) {
+	void MakeLeftSubTree(BTree<T>* bt) {  // 왼쪽에 서브 트리 연결. 이미 있다면 트리 삭제 및 메모리 소멸 후 연결
 		if (left != NULL) {
 			size -= left->size;
 			DeleteSubTree(left);
@@ -37,7 +37,7 @@ public:
 		size += bt->size;
 	}
 
-	void MakeRightSubTree(BTree<T>* bt) {
+	void MakeRightSubTree(BTree<T>* bt) {  // 오른쪽에 서브 트리 연결. 이미 있다면 트리 삭제 및 메모리 소멸 후 연결
 		if (right != NULL) {
 			size -= right->size;
 			DeleteSubTree(right);
@@ -47,46 +47,43 @@ public:
 		size += bt->size;
 	}
 
-	BTree<T>* RemoveLeftSubTree() {
+	BTree<T>* RemoveLeftSubTree() {  // 왼쪽 트리를 제거 후 메모리 소멸
 		BTree<T>* delNode = this->left;
 		this->left = NULL;
 
 		return delNode;
 	}
 
-	BTree<T>* RemoveRightSubTree() {
+	BTree<T>* RemoveRightSubTree() {  // 오른쪽 트리를 제거 후 메모리 소멸
 		BTree<T>* delNode = this->right;
 		this->right = NULL;
 
 		return delNode;
 	}
 
-	void ChangeLeftSubTree(BTree<T>* child) {
+	void ChangeLeftSubTree(BTree<T>* child) { // 메모리 소멸 없이 왼쪽 트리를 child로 변경
 		this->left = child;
 	}
 
-	void ChangeRightSubTree(BTree<T>* child) {
+	void ChangeRightSubTree(BTree<T>* child) { // 메모리 소멸 없이 오른쪽 트리를 child로 변경
 		this->right = child;
 	}
 
-	void ChangeData(T newData) {
+	void ChangeData(T newData) {  // 노드의 데이터 변경
 		data = newData;
 	}
 
-	T GetData() {
+	T GetData() {  // 노드 데이터 반환
 		return data;
 	}
 
-	int BTSize() {
+	int BTSize() { // this 포인터 트리의 노드 총 개수
 		return size;
 	}
-
-	friend void ChangeLeftSubTree(BTree<T>* parent, BTree<T>* child);
-	friend void ChangeRightSubTree(BTree<T>* parent, BTree<T>* child);
 };
 
 template <class T>
-void DeleteSubTree(BTree<T>* bt) {
+void DeleteSubTree(BTree<T>* bt) {  // 노드 제거 후 메모리 삭제까지 수행. bt를 루트로 하는 모든 트리 
 	if (bt == NULL) return;
 
 	DeleteSubTree(bt->GetLeftTree());
@@ -95,7 +92,7 @@ void DeleteSubTree(BTree<T>* bt) {
 }
 
 template <class T>
-void PreTrav(BTree<T>* nd, void (*action)(T data)) {
+void PreTrav(BTree<T>* nd, void (*action)(T data)) {  // 전위 순회
 	if (nd == NULL) return;
 
 	action(nd->GetData());
@@ -104,7 +101,7 @@ void PreTrav(BTree<T>* nd, void (*action)(T data)) {
 }
 
 template <class T>
-void InorTrav(BTree<T>* nd, void (*action)(T data)) {
+void InorTrav(BTree<T>* nd, void (*action)(T data)) {  // 중위 순회
 	if (nd == NULL) return;
 
 	InorTrav(nd->GetLeftTree(), action);
@@ -113,7 +110,7 @@ void InorTrav(BTree<T>* nd, void (*action)(T data)) {
 }
 
 template <class T>
-void PostTrav(BTree<T>* nd, void (*action)(T data)) {
+void PostTrav(BTree<T>* nd, void (*action)(T data)) {  // 후위 순회
 	if (nd == NULL) return;
 
 	PostTrav(nd->GetLeftTree(), action);
